cc950d2e60c710468103ac950e5898c223d9cc5f
Rainer Machne <r.machne@northeastern.edu>
2024-01-26 16:39:57 -0500

    trying to get s4pred to run

--- scripts/s4pred_run.sh
+++ scripts/s4pred_run.sh
@@ -12,10 +12,15 @@ spred=/home/${USER}/programs/s4pred/
 ## use awk to get range of sequences
 from=`echo $i  $seq_per_call | awk '{print $1 * $2}' -`
 to=`echo $from $seq_per_call | awk '{print $1 + $2 -1}' -`
-echo $i from $from to $to
+echo $i from $from to $to, with $ncpu cores
 
 ## retrieve sequences from fasta file
-srun awk "/^>/ {n++} (n>=$from && n<=$to) {print}" $fasta > s4pred_${i}.fas
+FILE=s4pred_${i}.fas
+if [ -f "$FILE" ]; then
+    echo use existing $FILE
+else
+    srun awk "/^>/ {n++} (n>=$from && n<=$to) {print}" $fasta > $FILE
+fi
 
 ## call python
-srun python ${spred}/run_model.py s4pred_${i}.fas --threads $ncpu -z -s -o /home/${USER}/data/mammary/processedData/s4pred/
+srun python ${spred}/run_model.py $FILE --threads $ncpu -z -s -o /home/${USER}/data/mammary/processedData/s4pred/
