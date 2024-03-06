3050b194070dd9b117f3d2ece77f30b6da2f8202
Rainer Machne <r.machne@northeastern.edu>
2024-03-06 16:49:33 -0500

    started some sequence context analysis around main identified sites

--- scripts/get_aas_context.R
+++ scripts/get_aas_context.R
@@ -5,6 +5,14 @@
 ## get sequences surrounding the AAS and analyze here,
 ## and export for sequence motif analysis.
 
+## TODO:
+## * filter unique proteins?
+
+library(segmenTools)
+require(ggplot2)
+require(ggseqlogo)
+
+
 ### PATHS AND FILES
 
 proj.path <- "/home/raim/data/mistrans"
@@ -19,7 +27,7 @@ in.file <- file.path(out.path,"saap_mapped3.tsv")
 ## protein fasta
 fasta <- file.path(out.path,"all_proteins.fa")
 ## coding region fasta
-transcr <- file.path(mam.path,"processedData","coding.fa")
+##transcr <- file.path(mam.path,"processedData","coding.fa")
 
 dat <- read.delim(in.file)
 
@@ -28,6 +36,10 @@ dat <- dat[!dat$exclude,]
 ## remove those w/o protein info
 dat <- dat[!is.na(dat$pos), ]
 
+## remove protein with substitutions at the same site
+## TODO: more complex handling of this
+dat <- dat[!duplicated(paste(dat$protein,dat$pos)),]
+           
 ## load fasta files, analyze frequencies, and write out
 ## fasta for sequence analysis.
 pfas <- readFASTA(fasta, grepID=TRUE)
@@ -58,21 +70,77 @@ pctx <- do.call(rbind, pctx)
 
 dat$fromto <- paste0(dat$from, ":", dat$to)
 
-filt <- dat$fromto=="Q:G"
-segmenTools::plotdev(file.path(fig.path,paste0("seqcontext_","Q:G")),
-                     height=3.5, width=3.5, res=300)
-par(mai=c(.5,.5,.1,.1), mgp=c(1.3,.3,0), tcl=-.25)
-plot(-dst:dst,apply(pctx[filt,],2,
-                    function(x) mean(x=="Q")), ylim=c(0,.5), type="l",
-     xlab="distance from AAS", ylab="AA frequency")
-abline(h=mean(pctx=="Q"))
-lines(-dst:dst,apply(pctx[filt,],2,
-                     function(x) mean(x=="N")), col=2)
-abline(h=mean(pctx=="N"),col=2)
-lines(-dst:dst,apply(pctx[filt,],2,
-                     function(x) mean(x=="A")), col=3)
-abline(h=mean(pctx=="A"),col=3)
-legend("topright", c("Q","N", "A"), col=c(1,2,3), lty=1)
-dev.off()
+filters <- rbind(c(column="fromto", pattern="S:G"),
+                 c(column="fromto", pattern="T:V"),
+                 c(column="fromto", pattern="Q:G"))
+                       
+for ( i in 1:nrow(filters) ) {
 
-table(pctx[filt,"-1"])
+    column <- filters[i,"column"]
+    pattern <- filters[i,"pattern"]
+    
+    filt <- dat[,column]==pattern
+    ctx <- pctx[filt,]
+
+    aa1 <- pattern
+    if ( length(grep(":",aa1)) ) 
+        aa1 <- strsplit(aa1,":")[[1]][1]
+
+    ## plot most frequent at -1 and +1
+    aa3 <-
+        names(which.max(table(ctx[,"1"])))
+    aa2 <-
+        names(which.max(table(ctx[,"-1"])))
+    
+    
+    segmenTools::plotdev(file.path(fig.path,paste0("seqcontext_",
+                                                   column,"_",pattern)),
+                         height=3.5, width=5, res=300)
+    par(mai=c(.5,.5,.1,.1), mgp=c(1.3,.3,0), tcl=-.25, yaxs="i")
+    plot(-dst:dst,apply(ctx,2, function(x) mean(x==aa1)), ylim=c(0,1), type="l",
+         xlab="distance from AAS", ylab="AA frequency")
+    axis(1, at=-1000:1000, tcl=par("tcl")/2, labels=FALSE)
+    abline(h=mean(pctx==aa1))
+    lines(-dst:dst,apply(ctx,2, function(x) mean(x==aa2)), col=2)
+    abline(h=mean(pctx==aa2),col=2)
+    lines(-dst:dst,apply(ctx,2, function(x) mean(x==aa3)), col=3)
+    abline(h=mean(pctx==aa3),col=3)
+    legend("topright", c(aa1,aa2, aa3), col=c(1,2,3), lty=1,
+           title=paste0("n=",sum(filt)))
+    figlabel(paste0(column,"==",pattern), pos="bottomleft")
+    dev.off()
+    
+    
+    ## SEQUENCE LOGO
+    ## generate position weight matrix
+    
+    
+    aaa <- unique(c(ctx))
+    aaa <- aaa[aaa!=""]
+    pwm <- matrix(0, ncol=ncol(ctx), nrow=length(aaa))
+    colnames(pwm) <- colnames(ctx)
+    rownames(pwm) <- aaa
+    
+    for ( j in 1:ncol(ctx) ) {
+        tb <- table(ctx[,j])
+        tb <- tb[names(tb)!=""]
+        pwm[names(tb),j] <- tb/sum(tb)
+    }
+    
+    plotdev(file.path(fig.path,paste0("seqcontext_",
+                                      column,"_",pattern,"_logo_bits")),
+            height=3.5, width=5, res=300)
+    par(mai=c(.5,.5,.1,.1), mgp=c(1.3,.3,0), tcl=-.25, yaxs="i")
+    ggseqlogo(pwm[,as.character(-10:10)], method="bits")
+    dev.off()
+    
+    plotdev(file.path(fig.path,paste0("seqcontext_",
+                                      column,"_",pattern,"_logo_prob")),
+            height=3.5, width=5, res=300)
+    par(mai=c(.5,.5,.1,.1), mgp=c(1.3,.3,0), tcl=-.25, yaxs="i")
+    ggseqlogo(pwm[,as.character(-10:10)], method="probability")
+    dev.off()
+    
+}
+
+apply(ctx[,23:29],1, paste, collapse="")
