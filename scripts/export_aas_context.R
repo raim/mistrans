f5c5433ce34a187d859439f51b0605983ad477d4
Rainer Machne <r.machne@northeastern.edu>
2024-04-10 16:18:36 -0400

    tested neural network approach

--- scripts/export_aas_context.R
+++ scripts/export_aas_context.R
@@ -16,9 +16,11 @@ library(seqinr)
 mam.path <- "/home/raim/data/mammary"
 proj.path <- "/home/raim/data/mistrans"
 dat.path <- file.path(proj.path,"processedData")
+fig.path <- file.path(proj.path,"figures","saap_context")
+dir.create(fig.path)
 
 ## DISTANCE AROUND AAS TO ANALYZE
-dst <- 10  # left/right distance for frequency plots
+DST <- 25  # left/right distance for frequency plots
 
 ## INPUT FILES
 ## list of SAAPs with coordinates
@@ -127,12 +129,12 @@ pctx <- sapply(1:nrow(dat), function(i) {
     fsq <- pfas[[i]]$seq # protein sequence
     nsq <- nchar(fsq)
     
-    rrng <- seq(-dst,dst,1)
+    rrng <- seq(-DST,DST,1)
     sq <- rep("-",length(rrng)) ## GAP
     names(sq) <- rrng
 
     ## GET RANGE AROUND AAS
-    rng <- (dat$pos[i]-dst):(dat$pos[i]+dst)
+    rng <- (dat$pos[i]-DST):(dat$pos[i]+DST)
     ## cut range to available
     rrng <- rrng[rng>0 & rng<=nsq]
     rng <- rng[rng>0 & rng<=nsq]
@@ -145,7 +147,7 @@ pctx <- do.call(rbind, pctx)
 
 ## gene names
 rownames(pctx) <-
-    paste0(dat$name,"_",dat$pos-dst,"_",dat$pos+dst)
+    paste0(dat$name,"_",dat$pos-DST,"_",dat$pos+DST)
 sum(pctx=="-")
 
 ## transcripts
@@ -154,13 +156,13 @@ tctx <- sapply(1:nrow(dat), function(i) {
     fsq <- tfas[[i]]$seq # protein sequence
     nsq <- nchar(fsq)
     
-    rrng <- seq(-dst*3,dst*3 +2,1)
+    rrng <- seq(-DST*3,DST*3 +2,1)
     sq <- rep("-",length(rrng)) ## GAP
     names(sq) <- rrng
 
     ## GET RANGE AROUND AAS
     tpos <- dat$pos[i]*3 -2
-    rng <- (tpos-dst*3):(tpos+dst*3 +2)
+    rng <- (tpos-DST*3):(tpos+DST*3 +2)
     ## cut range to available
     rrng <- rrng[rng>0 & rng<=nsq]
     rng <- rng[rng>0 & rng<=nsq]
@@ -173,7 +175,7 @@ tctx <- do.call(rbind, tctx)
 
 ## gene names
 rownames(tctx) <-
-    paste0(dat$name,"_",dat$pos-dst,"_",dat$pos+dst)
+    paste0(dat$name,"_",dat$pos-DST,"_",dat$pos+DST)
 sum(tctx=="-")
 
 ## QC: seqinr::translate transcripts and compare to proteins
@@ -223,7 +225,7 @@ unex <- unex[!unex%in% c(isu,isx)]
 cat(paste(length(unex),
           "non-matching sequences remaining\n"))
 if ( length(unex)!=0 )
-    stop("manual inspection of non-matching transcript and protein sequences",
+    warning("do manual inspection of non-matching transcript and protein seqs",
          "is not valid anymore - RECHECK")
 
 ## ADD RAAS STATS
@@ -250,27 +252,73 @@ tmts <- lapply(tmtl, function(x) {
 })
 tmts <- as.data.frame(do.call(rbind, tmts))
 
+## TODO: vary over context dst=25 to 0
+## w and w/o AAS, and for peptides and codons.
+
 ## RESULTS
+dst <- 25
+omit.aas <- c() #-20:20
 plim <- as.character(-dst:dst)
-plim <- plim[plim!="0"]
-res <- cbind(BP=dat$BP,
-             SAAP=dat$SAAP,
-             peptide=gsub("-","Z",apply(pctx[,plim], 1, paste, collapse="")),
-             codons=apply(tctx, 1, paste, collapse=""),
+tlim <- as.character(-(3*dst):(3*dst+2))
+if ( length(omit.aas)>0 ) {
+    plim <- plim[!plim%in%as.character(omit.aas)]
+    ## TODO: expand to codons
+    omit.codons <- (3*min(omit.aas)):(3*max(omit.aas))
+    tlim <- tlim[!tlim%in%as.character(omit.codons)]
+}
+res <- cbind(BP=dat$BP, SAAP=dat$SAAP,
+             peptide=gsub("-","Z",apply(pctx[,plim, drop=FALSE],
+                                        1, paste, collapse="")),
+             codon=apply(tctx[,tlim, drop=FALSE], 1, paste, collapse=""),
              tmts)
 rownames(res) <- NULL
 
+#plim
 ## remove all with NAN/Inf median
 rmv <- is.infinite(res$median) | is.na(res$median)
 ## remove all immunoglobulins
 isig <- dat$IG
+## remove duplicates
+dups <- duplicated(paste(dat$BP))#, dat$SAAP))
 
-res <- res[!(isig|rmv),]
+## REMOVE IG, inf/na RAAS and duplicate BP
+res <- res[!(isig|rmv|dups),]
+
+## TODO: randomize RAAS associations
+randomize <- FALSE #TRUE#
+if ( randomize ) {
+    res$median <-
+        sample(res$median)
+}
+
+randomize.peptides <- FALSE #TRUE
+if ( randomize.peptides ) {
+    peps <- strsplit(res$peptide,"")
+    pepr <- unlist(lapply(peps, function(x) paste(sample(x),collapse="")))
+    res$peptide <- pepr
+}
+
+sum(!is.na(res$cv))
+hist(res$cv)
 
 out.file <- file.path(dat.path, "saap_context.tsv")
 write.table(res, file=out.file, sep="\t", quote=FALSE, na="", row.names=FALSE)
 
-
 ## TODO:
-## filter input: rm U and X containing sequences.
-## 
+## run python/keras here on commandline
+
+
+if ( FALSE ) { # only do when sure that desired results are present
+    res.file <- file.path(dat.path, "saap_context_prediction.tsv")
+    prd <- read.delim(res.file, row.names=1)
+    fig.file <- file.path(fig.path, "saap_context_dnn")
+    plotdev(fig.file, width=3, height=3, res=200)
+    par(mai=c(.5,.5,.1,.1), mgp=c(1.3,.3,0), tcl=-.25)
+    plotCor(prd[,1], prd[,2], xlab=expression(log[10]*bar(RAAS)),
+        ylab="prediction", round=2)
+    dev.off()
+}
+
+## NOTE: best correlation ~0.3 with tight context around AAS,
+## still significant correlation at <0.2 w/o central but even
+## without -20:20!
