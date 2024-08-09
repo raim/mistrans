ded01e5eb61d71c1d77733d15ef8fce2f58bebfe
raim <raim@tbi.univie.ac.at>
2024-08-09 09:09:52 -0400

    calculating overlap of psi with all codon positions

--- scripts/raasprofiles3_rna.R
+++ scripts/raasprofiles3_rna.R
@@ -30,25 +30,40 @@ names(chrIdx) <- chrMap[,2]
 psi.file <- file.path(dat.path, "six_cell_lines_minimal(A549).csv")
 
 psi <- read.csv(psi.file)
-
 colnames(psi) <- sub("position","coor", colnames(psi))
 
-## convert chromosomes ID to index
+hist(psi$mm.Direct)
+
+## convert chromosomes ID to index via segmenTools'
+## chromosome coordinate indexing system.
 psi$chr <-  chrIdx[sub("^M$","MT",sub("chr","",psi$chr))]
 bdat$chr <- chrIdx[bdat$chr]
 
 psidx <- coor2index(psi[,c("chr","coor")], chrS=chrS)
-bdidx <- coor2index(bdat[,c("chr","coor")], chrS=chrS)
+bd2dx <- coor2index(bdat[,c("chr","coor")], chrS=chrS)
+bd1 <- cbind(chr=bdat[,c("chr")],
+             coor=bdat[,c("coor")] + ifelse(bdat[,"strand"]=="+",-1,1))
+bd1dx <- coor2index(bd1, chrS=chrS)
+bd3 <- cbind(chr=bdat[,c("chr")],
+             coor=bdat[,c("coor")] - ifelse(bdat[,"strand"]=="+",-1,1))
+bd3dx <- coor2index(bd3, chrS=chrS)
 
-bdat[which(bdidx[,2]%in%intersect(psidx[,2], bdidx[,2])),"name"]
+##bdat[which(bd2dx[,2]%in%intersect(psidx[,2], bd2dx[,2])),"name"]
 
-##dstm <- dist(psidx[,2], bdidx[,2])
-bid <- which(bdidx[,2]%in%intersect(psidx[,2], bdidx[,2]))
-pid <- which(psidx[,2]%in%intersect(psidx[,2], bdidx[,2]))
+##dstm <- dist(psidx[,2], bd2dx[,2])
+bid <- which(bd3dx[,2]%in%intersect(psidx[,2], bd3dx[,2]))
+pid <- which(psidx[,2]%in%intersect(psidx[,2], bd3dx[,2]))
 
 unique(bdat[bid,"name"])
+bdat[bid,"RAAS"]
+
 unique(psi[pid,"Annotation"])
+psi[pid,"mm.Direct"]
+
 table(bdat[bid,"fromto"])
 
-bdat[bid,c("chr","coor")]
-psi[pid,c("chr","coor")]
+##bdat[bid,c("chr","coor","strand")]
+##psi[pid,c("chr","coor")]
+
+length(bid)
+length(pid)
