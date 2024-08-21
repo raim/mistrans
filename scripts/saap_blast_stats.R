30069576ae695c8ccef79a724159e915ee371f2f
Rainer Machne <r.machne@northeastern.edu>
2024-08-21 18:15:18 -0400

    fixed SAAP blast interpretation, but ugly unfinished script

--- scripts/saap_blast_stats.R
+++ scripts/saap_blast_stats.R
@@ -1,6 +1,9 @@
 library(segmenTools)
 options(stringsAsFactors=FALSE)
 
+## TEST SAAP BLASTED AGAINST ALL ENSEMBL PROTEINS
+## incl. patient-specific mutations
+
 mam.path <- "/home/raim/data/mammary"
 proj.path <- "/home/raim/data/mistrans"
 dat.path <- file.path(proj.path,"originalData")
@@ -106,40 +109,4 @@ legend("topright", c("all SAAP",
        col=c(8,2), lty=1, lwd=5)
 dev.off()
 
-table(tmtf$Keep.SAAP[tmtf$SAAP %in% unique(perfect$SAAP)])
 table(tmtf$AAS[tmtf$Keep.SAAP & tmtf$SAAP %in% unique(perfect$SAAP)])
-
-## TODO: sort for best hits and remove duplicates as in get_protein_match.R
-## TODO: fuse this into get_protein_match.R or map_peptides3.R
-## SORT & FILTER
-## sort by
-## 1) identity,
-## 2) -mismatches (perhaps redundant?),
-## x) whether protein is the annotated MANE, and
-## x) number of annotations.
-## x) remove duplicated after sorting!
-saap <- saap[order(saap$identity, saap$alen, 
-                   -saap$gaps, -saap$mismatches, decreasing=TRUE),]
-
-## check sorting for first entries
-tid <- saap$SAAP[which(duplicated(saap$SAAP))[1]]
-
-head(saap[saap$SAAP==tid,])
-
-## filter all where alignment length equals query (SAAP) length,
-## w/o gaps, then count mismatches
-
-
-
-## remove duplicated, which should appear below best hits!
-saap <- saap[!duplicated(paste0(saap$SAAP,saap$gene)),]
-
-plotdev(file.path(fig.path,"saap_blast"),
-        res=300, width=7, height=3.5, type=ftyp)
-par(mfcol=c(1,2), mai=c(.5,.5,.1,.1), mgp=c(1.3,.3,0), tcl=-.25)
-hist(saap$identity)
-bp <- table(saap$mismatches)
-barplot(bp, xlab="number of mismatches")
-dev.off()
-
-pm <- saap[which(saap$mismatches==0),]
