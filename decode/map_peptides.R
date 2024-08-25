8232ce929b16e2dc8d127596255385455c047a08
raim <raim@tbi.univie.ac.at>
2024-08-25 16:43:06 -0400

    added genetic code to replace biostrings dependence

--- decode/map_peptides.R
+++ decode/map_peptides.R
@@ -95,6 +95,32 @@ pfdl.file <- file.path(mam.path,"originalData", "9606.tsv.gz")
 ## pfam clans
 clans.file <- file.path(mam.path,"originalData", "pfam", "Pfam-A.clans.tsv.gz")
 
+## copied from the Biostrings package
+GENETIC_CODE <- c(
+    TTT="F", TTC="F",         
+    TTA="L", TTG="L",         
+    TCT="S", TCC="S", TCA="S", TCG="S", AGT="S", AGC="S",         
+    TAT="Y", TAC="Y",         
+    TAA="*", TAG="*", TGA="*",
+    TGT="C", TGC="C",    
+    TGG="W",         
+    CTT="L", CTC="L", CTA="L", CTG="L",         
+    CCT="P", CCC="P", CCA="P", CCG="P",         
+    CAT="H", CAC="H",         
+    CAA="Q", CAG="Q",         
+    CGT="R", CGC="R", CGA="R", CGG="R", AGA="R", AGG="R",         
+    ATT="I", ATC="I", ATA="I",
+    ATG="M",         
+    ACT="T", ACC="T", ACA="T", ACG="T",         
+    AAT="N", AAC="N",         
+    AAA="K", AAG="K",         
+    GTT="V", GTC="V", GTA="V", GTG="V",         
+    GCT="A", GCC="A", GCA="A", GCG="A",         
+    GAT="D", GAC="D",         
+    GAA="E", GAG="E",         
+    GGT="G", GGC="G", GGA="G", GGG="G"
+)
+
 ## figures
 dir.create(fig.path, showWarnings=FALSE)
 
