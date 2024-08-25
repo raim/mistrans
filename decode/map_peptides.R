76ba597c310e863269077fce37a79f7409b10765
raim <raim@tbi.univie.ac.at>
2024-08-25 12:28:06 -0400

    message on saap_mapped.tsv

--- decode/map_peptides.R
+++ decode/map_peptides.R
@@ -41,6 +41,8 @@ if ( !file.exists(feature.file) )
     stop("genome feature table file not found, this script requires",
          "setup of genomic data via the genomeBrowser/data/mammary/setup.sh.",
          "If you have setup this, please provide the path here as `mam.path`",
+         "and either change the path of the saap_mapped.tsv input or copy",
+         "it from processedData to additionalData."
          "NOTE that the R analysis can still be run, since we provide",
          "the output of this script, saap_mapped.tsv")
 
