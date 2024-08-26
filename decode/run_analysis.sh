4b4f1d3b83a5b8333adecc5c8dc46ede7a1b48be
raim <raim@tbi.univie.ac.at>
2024-08-26 19:33:53 -0400

    complete figure references for rscript calls in run_analaysis.sh

--- decode/run_analysis.sh
+++ decode/run_analysis.sh
@@ -20,13 +20,13 @@ fi
 R --vanilla <  raas_init.R &> $DECODE/log/raas_init.txt  
 
 ### CALCULATE RAAS PROFILES and GENERATE PUBLICATION FIGURES
-R --vanilla < codons.R &> $DECODE/log/codons.txt  # FIGURE 2
-R --vanilla < aminoacids.R &> $DECODE/log/aminoacids.txt  # FIGURE 3
-R --vanilla < motifs.R &> $DECODE/log/motifs.txt  # FIGURE 4, FIGURE 5
-R --vanilla < function.R &> $DECODE/log/function.txt # FIGURE 4
-R --vanilla < kraq.R &> $DECODE/log/kraq.txt  # Ext. Data Figure 8: motifs
-R --vanilla < proteins.R &> $DECODE/log/proteins.txt # FIGURE 5
-R --vanilla < structure.R &> $DECODE/log/structure.txt  # Ext. Data Figure 10
-R --vanilla < rna.R &> $DECODE/log/rna.txt # Suppl. Figure 2
+R --vanilla < codons.R &> $DECODE/log/codons.txt  # Fig. 2j, EFig. 3k, 4
+R --vanilla < aminoacids.R &> $DECODE/log/aminoacids.txt  # Fig. 3b,c,e, EFig. 5a,c,f
+R --vanilla < function.R &> $DECODE/log/function.txt # Fig. 4c, EFig. 7
+R --vanilla < motifs.R &> $DECODE/log/motifs.txt  # Fig. 4c,d, Fig. 5c, EFig. 8a,c
+R --vanilla < kraq.R &> $DECODE/log/kraq.txt  # EFig. 8b
+R --vanilla < proteins.R &> $DECODE/log/proteins.txt # Fig. 5a,b, EFig. 10a
+R --vanilla < structure.R &> $DECODE/log/structure.txt  # EFig. 10b,c
+R --vanilla < rna.R &> $DECODE/log/rna.txt # SFig. 2
 
 ## TODO: add 1d protein plots and chimeraX generating code
