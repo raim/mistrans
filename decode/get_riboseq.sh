77db6f939174d0eaaf67634ae3977ed692a7dd54
raim <raim@tbi.univie.ac.at>
2024-08-29 10:37:14 -0400

    proceding a bit with riboseq data

--- decode/get_riboseq.sh
+++ decode/get_riboseq.sh
@@ -8,10 +8,17 @@ myBWTBG=~/programs/ucsc_utils/bigWigToBedGraph
 
 ## manual download from https://gwips.ucc.ie/downloads/index.html
 ## mammalian > human
+
 ## Elongating Ribosomes (A Site) > Global Aggregate >
 ## Iwasaki19_RiboProElong_global_(child) -> DOWNLOAD
 
-## mv ~/Downloads/Iwasaki19_All.RiboProElong.bw $DECODE/originalData
+## Elongating Ribosomes (Footprints) > Global Aggregate >
+## Iwasaki16_All.RiboCov.bw -> DOWNLOAD
 
-$myBWTBG $DECDE/originalData/Iwasaki19_All.RiboProElong.bw $DECDE/processedData/Iwasaki19_All.RiboProElong.bed
+## mv ~/Downloads/Iwasaki19_All.RiboProElong.bw $DECODE/originalData
+## mv ~/Downloads/Iwasaki16_All.RiboCov.bw $DECODE/originalData
+
+$myBWTBG $DECODE/originalData/Iwasaki19_All.RiboProElong.bw $DECODE/processedData/Iwasaki19_All.RiboProElong.bed
+$myBWTBG $DECODE/originalData/Iwasaki19_All.RiboProElong.bw $DECODE/processedData/Iwasaki19_All.RiboProElong.bed
+
 
