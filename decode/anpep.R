ba197ba682c3b8b6d80f4c68814a5f69af89be07
Rainer Machne <r.machne@northeastern.edu>
2024-09-12 15:28:06 -0400

    added intracellular peptidase

--- decode/anpep.R
+++ decode/anpep.R
@@ -10,13 +10,14 @@ colnames(dat)
 nrm <- dat[,3:31]
 nrm <- t(t(nrm)/apply(nrm,2,sum, na.rm=TRUE))
 
-gidx <- dat$"Gene name" == "ANPEP"
+prts <- rbind(ANPEP=nrm[dat$"Gene name" == "ANPEP",],
+              NPEPPS=nrm[dat$"Gene name" == "NPEPPS",])
 
 png(file.path(fig.path,"aminopeptidase_N_bytissue.png",
               units="in", width=10, height=5, res=300)
 par(mai=c(1.6,1,.1,.1), mgp=c(1.3,.3,0), tcl=-.25)
-barplot(nrm[gidx,], las=2)
+barplot(prts, las=2, beside=TRUE, legend=TRUE)
 mtext(expression(I[ANPEP]/I[total]), 2, 3.3)
-legend("topright", "ANPEP: aminopeptidase N")
+#legend("topright", "ANPEP: aminopeptidase N")
 dev.off()
 
