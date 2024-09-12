8208b37af69aacbf723397934551f9131fa22767
raim <raim@intron>
2024-09-12 10:02:41 -0400

    added anpep analysis in tissue proteomes by wang et al. 2019

---
+++ decode/anpep.R
@@ -0,0 +1,20 @@
+library(readxl)
+
+infile <- '~/data/mammary/originalData/wang19_table_EV1.xlsx'
+
+dat <- read_xlsx(infile, sheet=4)
+
+colnames(dat)
+
+nrm <- dat[,3:31]
+nrm <- t(t(nrm)/apply(nrm,2,sum, na.rm=TRUE))
+
+gidx <- dat$"Gene name" == "ANPEP"
+
+png("aminopeptidase_N_bytissue.png", units="in", width=10, height=5, res=300)
+par(mai=c(1.6,1,.1,.1), mgp=c(1.3,.3,0), tcl=-.25)
+barplot(nrm[gidx,], las=2)
+mtext(expression(I[ANPEP]/I[total]), 2, 3.3)
+legend("topright", "ANPEP: aminopeptidase N")
+dev.off()
+
