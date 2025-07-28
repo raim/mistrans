41e87f53bd9bbf47223ecfc22320ae7d41652863
raim <raim@intron>
2025-07-28 18:11:29 +0200

    cleaned codon frequencies

--- genome/codon_frequency.R
+++ genome/codon_frequency.R
@@ -6,40 +6,21 @@ library(coRdon)
 
 ## codon frequency from generated coding region fasta
 mam.path <- Sys.getenv("MAMDATA")
-transcr <- file.path(mam.path,"processedData","coding.fa")#"Homo_sapiens.GRCh38.
+
+transcr <- file.path(mam.path, "processedData", "coding.fa")
+
 out.file <- sub("\\.fa", "_codons.tsv", transcr)
+
+## read transcripts fasta
 seq <- readSet(file=transcr)
+## generate codon table
 cod <- codonTable(seq)
 
 cc <- codonCounts(cod)
 rownames(cc) <- sub(",.*", "", names(seq))
 
+## write out global codon frequencies
 out <- cbind.data.frame(ID=rownames(cc), cc)
 write.table(out, file=out.file, sep="\t", row.names=FALSE, quote=FALSE)
 
-## AA frequency
 
-
-if ( FALSE ) {
-
-    library(segmenTools)
-    ## test coRdon: 
-
-    dnaHD59 <- readSet(
-        file="https://raw.githubusercontent.com/BioinfoHR/coRdon-examples/master/HD59.fasta"
-    )
-    HD59 <- codonTable(dnaHD59)
-    
-    xlab <- "MILC distance from sample centroid"
-    ylab <- "MILC distance from ribosomal genes"
-    milc <- MILC(HD59, ribosomal = TRUE)
-    
-    dense2d(milc[,1], milc[,2])
-
-    genes <- getKO(HD59)[getlen(HD59) > 80]
-    
-    subset <- list(half=c(rep(TRUE,20), rep(FALSE,length(seq)-20)))
-    
-    milc <- MILC(cod, self = FALSE,
-                 subsets = subset)
-}
