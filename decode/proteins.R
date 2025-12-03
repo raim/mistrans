b6ebc5db20e3457c9ba2dd30982afdf0bf9fe9ef
raim <rainer.machne@gmail.com>
2025-12-03 13:54:46 +0100

    refined new bp/saap/abundance plot

--- decode/proteins.R
+++ decode/proteins.R
@@ -264,20 +264,25 @@ plotdev(file.path(pfig.path,paste0("protein_correlations")),
         type=ftyp, res=300, width=corW*5,height=corH)
 par(mfcol=c(1,5), mai=c(.35,.35,.1,.1), mgp=pmpg, tcl=-.25)
 
-## BP and SAAP are correlated
-plotCor(tmtf$BP.abundance, tmtf$SAAP.abundance, log='xy',
-        ylab='SAAP abundance', xlab='BP abundance')
-## SAAP is not correlated to protein intensity
-plotCor(tmtf$protein.intensity, tmtf$SAAP.abundance, log='xy',
-        xlab='protein intensity', ylab='SAAP abundance')
 ## BP is correlated to protein intensity
 plotCor(tmtf$protein.intensity, tmtf$BP.abundance, log='xy',
         xlab='protein intensity', ylab='BP abundance')
-
+## 1/BP is correlated to protein intensity
 plotCor(tmtf$protein.intensity, 1/tmtf$BP.abundance, log='xy',
         xlab='protein intensity', ylab='1 / BP abundance')
-
+##figlabel('C', pos='topleft', font=2, cex=1.2)
+##figlabel('A', pos='topleft', font=2, cex=1.2)
+## SAAP is not correlated to protein intensity
+plotCor(tmtf$protein.intensity, tmtf$SAAP.abundance, log='xy',
+        xlab='protein intensity', ylab='SAAP abundance')
+##figlabel('B', pos='topleft', font=2, cex=1.2)
+## SAAP/BP is correlated to protein intensity
 plotCor(tmtf$protein.intensity, tmtf$SAAP.abundance/tmtf$BP.abundance, log='xy',
         xlab='protein intensity', ylab='RAAS')
+##figlabel('D', pos='topleft', font=2, cex=1.2)
+## BP and SAAP are correlated
+plotCor(tmtf$BP.abundance, tmtf$SAAP.abundance, log='xy',
+        ylab='SAAP abundance', xlab='BP abundance')
+##figlabel('E', pos='topleft', font=2, cex=1.2)
 dev.off()
 
